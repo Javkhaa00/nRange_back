7adf5aa688362068bc2baae4087b4fcb3f0cd5bd
Javkhaa <Javkhaa@Oyunbaatars-iMac.local>
2021-03-05 17:11:18 +0800

    feat: build update

---
+++ public/precache-manifest.31abeb06c1663af8c11535862515fa0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "012297b487f3b285975456d3f1f3e806",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22d44e0c964f303aab49",
+    "url": "/static/css/main.593abcd1.chunk.css"
+  },
+  {
+    "revision": "bc2d0f6517c5448c75de",
+    "url": "/static/js/2.969eaddc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.969eaddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22d44e0c964f303aab49",
+    "url": "/static/js/main.089ff42c.chunk.js"
+  },
+  {
+    "revision": "2d3e85f110cce97c6277",
+    "url": "/static/js/runtime-main.79a773e5.js"
+  }
+]);
